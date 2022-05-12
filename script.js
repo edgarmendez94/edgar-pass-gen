23c9e1b7a853d1f02d0c4a16c99cf1403a1d1c3f
Edgar Mendez <edgar.ym26@gmail.com>
2022-05-12 01:19:56 -0400

    finished if else things

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var specialChars = ["!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , 
 var digits = [ "0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"];
 var lowerChar = ['a','b','c','d','e','f','g','h','i','j',"k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
+var selection = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +12,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
  
   passwordText.value = password;
+
+  for (var i = 0; i < passwordTextLength; i++) {
+    
+    
+    
+}
+
 
 }
 
@@ -29,12 +36,26 @@ var willHaveUpper = prompt("Will your password have upper case letters? type yes
 var willHaveLower = prompt("Will your password have lower case letters? type yes or no")
    
 if (willHaveSpecial === yes && willHaveUpper === yes && willHaveLower === yes ){
-
-  
+    selection = specialChars.concat(digits,lowerChar,upperChar)
+    
 }else if (willHaveSpecial === yes && willHaveUpper === no && willHaveLower === yes){   
-
+    selection = specialChars.concat(digits,lowerChar,)
 }
-
+else if (willHaveSpecial === yes && willHaveUpper === no && willHaveLower === no){   
+    selection = specialChars.concat(digits)
+}
+else if (willHaveSpecial === no && willHaveUpper === no && willHaveLower === no){   
+    selection = digits
+}
+else if (willHaveSpecial === no && willHaveUpper === yes && willHaveLower === yes){   
+    selection = upperChar.concat(digits,lowerChar,)
+}
+else if (willHaveSpecial === yes && willHaveUpper === yes && willHaveLower === no){   
+    selection = lowerChar.concat(digits)
+}
+else if (willHaveSpecial === no && willHaveUpper === yes && willHaveLower === no){   
+    selection = specialChars.concat(digits,lowerChar,)
+}
 
 
 }
@@ -42,10 +63,7 @@ if (willHaveSpecial === yes && willHaveUpper === yes && willHaveLower === yes ){
  
 
 
-// for (var i = 0; i < passwordTextLength; i++) {
-    
-//     console.log("This is the current value of i: " + i + ".");
-// }
+
 
 
 // Add event listener to generate button
